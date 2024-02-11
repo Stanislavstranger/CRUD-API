ca66d72a323d9d75e4aa6ff5db1b17b6888831c6
Stanislav <stanislavstranger@gmail.com>
2024-02-12 02:13:54 +0300

    init: start CRUD-API project

---
+++ webpack.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+
+module.exports = {
+	mode: 'development',
+	entry: path.resolve(__dirname, 'server.ts'),
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/
+			}
+		]
+	}
+};
