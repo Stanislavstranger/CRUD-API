347a7f6c4239c48976c850105fc4db8cf791a917
Stanislav <stanislavstranger@gmail.com>
2024-02-12 16:57:31 +0300

    feat: add handleGetUserByID and handleCreateUser

--- server.ts
+++ server.ts
@@ -2,15 +2,33 @@ import 'dotenv/config';
 import 'colors';
 import { createServer } from 'http';
 import { parse } from 'url';
-import { deleteUserById, getUsers } from './src/service/userService';
-import { handleGetUsers } from './src/controllers/userController/userController';
+import {
+	handleCreateUser,
+	handleGetUserById,
+	handleGetUsers
+} from './src/controllers/userController/userController';
 
 const server = createServer((req, res) => {
 	const url = parse(req.url as string, true);
 	const { pathname, query } = url;
+	console.log(url);
 
 	if (pathname === '/api/users' && req.method === 'GET') {
 		handleGetUsers(req, res);
+	} else if (pathname === '/api/users' && req.method === 'POST') {
+		let data = '';
+		req.on('data', (chunk) => {
+			data += chunk;
+		});
+		req.on('end', () => {
+			const userData = JSON.parse(data);
+			handleCreateUser(req, res, userData);
+		});
+	} else if (pathname === '/api/users' && req.method === 'GET' && query.userId) {
+		handleGetUserById(req, res, query.userId as string);
+	} else {
+		res.writeHead(404, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({ message: 'Not found' }));
 	}
 });
 
@@ -19,7 +37,3 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`.blue.inverse);
 });
-
-console.log(getUsers());
-console.log(deleteUserById('2'));
-console.log(getUsers());
--- src/controllers/userController/userController.ts
+++ src/controllers/userController/userController.ts
@@ -1,5 +1,6 @@
 import { ServerResponse, IncomingMessage } from 'http';
-import { getUsers } from '../../service/userService';
+import { getUsers, getUserById, createUser as createNewUser } from '../../service/userService';
+import { User } from '../../models/user';
 
 const sendResponse = (res: ServerResponse, statusCode: number, data: any) => {
 	res.writeHead(statusCode, { 'Content-Type': 'application/json' });
@@ -12,5 +13,35 @@ export const handleGetUsers = async (req: IncomingMessage, res: ServerResponse) 
 		sendResponse(res, 200, users);
 	} catch (error) {
 		sendResponse(res, 500, { message: 'Internal Server Error' });
+	}
+};
+
+export const handleGetUserById = async (
+	req: IncomingMessage,
+	res: ServerResponse,
+	userId: string
+) => {
+	try {
+		const user = await getUserById(userId);
+		if (!user) {
+			sendResponse(res, 404, { message: 'User not found' });
+			return;
+		}
+		sendResponse(res, 200, user);
+	} catch (error) {
+		sendResponse(res, 500, { message: 'Internal Server Error' });
+	}
+};
+
+export const handleCreateUser = async (
+	req: IncomingMessage,
+	res: ServerResponse,
+	userData: User
+) => {
+	try {
+		const newUser = await createNewUser(userData);
+		sendResponse(res, 201, newUser);
+	} catch (error) {
+		sendResponse(res, 404, { message: 'Bad Request' });
 	}
 };
